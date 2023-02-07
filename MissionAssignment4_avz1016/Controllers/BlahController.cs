922a1dd9ad6c566147e9b564f3619780f5922ab4
lexizickella <lexizickella@LEXIZICKELLB450>
2023-02-06 22:42:02 -0700

    Assignment 5

---
+++ MissionAssignment4_avz1016/Controllers/BlahController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MissionAssignment4_avz1016.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionAssignment4_avz1016.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Inputs ()
+        {
+            return View();
+        }
+        [HttpPost] // attribute we are attaching to the method
+        public IActionResult Inputs (InputsModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MissionAssignment4_avz1016/Models/InputsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionAssignment4_avz1016.Models
+{
+    public class InputsModel
+    {
+        [Required]
+        public string assignments { get; set; }
+        [Required]
+        public string groupProject { get; set; }
+        [Required]
+        public string quizzes { get; set; }
+        [Required]
+        public string midtermExam { get; set; }
+        [Required]
+        public string finalExam { get; set; }
+        [Required]
+        public string intex { get; set; }
+    }
+
+}
